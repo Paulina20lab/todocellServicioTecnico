3a15849f239c5f1a8f660d2f5803b716b29c293d
Paulina Elizabeth Azuero Ambrosi <131897131+Paulina20lab@users.noreply.github.com>
2023-08-10 15:51:12 -0500

    Se sube el archivo

---
+++ servicioTecnico/src/main/kotlin/com/todocell/servicioTecnico/ServicioTecnicoApplication.kt
@@ -0,0 +1,11 @@
+package com.todocell.servicioTecnico
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServicioTecnicoApplication
+
+fun main(args: Array<String>) {
+	runApplication<ServicioTecnicoApplication>(*args)
+}
